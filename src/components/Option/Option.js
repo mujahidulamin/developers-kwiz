e8855ee06caacbe5bbf8734bb4f9c87786c29ac6
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-10-11 03:42:52 +0600

    correct-answer-and-wrong-answer-functionality-added

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import './Option.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Option = ({ option }) => {
+const Option = ({ option, question }) => {
     console.log(option);
+        const {correctAnswer} = question
+        // console.log(correctAnswer);
+    const handleAnswerQuestion = () => {
+        if(correctAnswer === option){
+            toast.success('Your Answer is Correct', {autoClose: 500})
+        }else{
+            toast.error('Your Answer is Incorrect', {autoClose: 500})
+        }
+    }
+
     return (
-        <div className='bg-white my-3 rounded p-2'>
+        <div className='bg-white my-3 rounded p-2 hover'>
             <label className='radio'>
-                <input className='mr-2' type="radio" value="Male" name="gender" />{option}
+                <ToastContainer position='top-center'></ToastContainer>
+                <input onClick={handleAnswerQuestion} className='mr-2' type="radio" value="Male" name="gender" />{option}
             </label>
         </div>
     );
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import Option from '../Option/Option'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Question = ({ question }) => {
     console.log(question);
+    const {correctAnswer} = question
+    const handleRightAnswer = () => {
+        toast.info(correctAnswer, {autoClose: 500})
+    }
+
+
     return (
         <div className='bg-cyan-200 p-5 w-9/12 m-auto my-4 rounded-3xl'>
-            <p className='text-center'>Quiz: {question.question}</p>
+            <ToastContainer position='top-center'></ToastContainer>
+            <div className='flex justify-center'>
+                <p className='text-center'>Quiz: {question.question}
+                </p>
+               <button onClick={handleRightAnswer}><i className="fa-solid fa-eye ml-16"></i></button>
+            </div>
+
             <div className=' text-center my-8 gap-5 grid grid-cols-2'>
-            { 
-                question.options.map(option => <Option
-                    option = {option}
-                ></Option>)
-            }
+                {
+                    question.options.map(option => <Option
+                        option={option}
+                        question={question}
+                    ></Option>)
+                }
 
             </div>
         </div>
